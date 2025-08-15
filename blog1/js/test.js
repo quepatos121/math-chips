fb6d65c809f7aab9700894552dfed32a57a6db9e
quepatos121 <162911102+quepatos121@users.noreply.github.com>
2025-08-15 14:43:48 -0600

    test

--- blog1/js/test.js
+++ blog1/js/test.js
@@ -1,4 +1,4 @@
-const btn = document.querySelector("button");
+const btn = document.querySelector(".generator");
 btn.addEventListener("click", () => {
   btn.textContent = "¡¡ME HICISTE CLIC!! ❤️";
   setTimeout(() => {
--- blog1/test.js
+++ blog1/test.js
@@ -1,7 +1,7 @@
 
 
 
-const btn = document.querySelector("generator");
+const btn = document.querySelector(".generator");
 btn.addEventListener("click", () => {
   btn.textContent = "¡¡ME HICISTE CLIC!! ❤️";
   setTimeout(() => {

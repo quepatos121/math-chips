fafeea9e62309d542e30ddbf3dfade81b29dc94a
quepatos121 <162911102+quepatos121@users.noreply.github.com>
2025-08-15 14:28:58 -0600

    Update test.js

--- blog1/test.js
+++ blog1/test.js
@@ -1,10 +1,11 @@
 
 
 
-var btn = document.querySelector("button");
+const btn = haiku.querySelector("button");
 btn.addEventListener("click", () => {
   btn.textContent = "¡¡ME HICISTE CLIC!! ❤️";
   setTimeout(() => {
     btn.textContent = "Hai ku generator";
   }, 1000);
 });
+
